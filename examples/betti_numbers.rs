c782e41b3280f8a491b736656577a6cbcb122f21
Amber Thrall <amber.rose.thrall@gmail.com>
2024-03-21 16:38:42 -0700

    betti numbers example loads in complex from class

--- examples/betti_numbers.rs
+++ examples/betti_numbers.rs
@@ -1,13 +1,33 @@
 extern crate scomplex;
 use scomplex::*;
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
 use std::time::Instant;
 
 fn main() {
-    let mut complex = Complex::new();
-    complex.push(splx![0,1,2]);
-    complex.push(splx![1,3,4]);
-    complex.push(splx![2,3]);
+    // Load in the data
+    let file = File::open("examples/triangles.txt").expect("failed to open triangles file");
+    let reader = BufReader::new(file);
+    let mut triangles = Vec::new();
 
+    for line in reader.lines() {
+        let indices: Vec<usize> = line.unwrap()
+            .split_whitespace()
+            .map(|x| x.parse::<usize>().expect("failed to parse data"))
+            .collect();
+        triangles.push(indices);
+    }
+
+    // Construction of complex
+    let now = Instant::now();
+    let mut complex = Complex::new();
+    for tri in triangles {
+        complex.push(Simplex::from(tri));
+    }
+    let elapsed = now.elapsed();
+    println!("Construction: {:.2?}", elapsed);
+
+    // Betti numbers
     let mut euler_characteristic = 0;
     for p in 0..=complex.dim() {
         let now = Instant::now();
