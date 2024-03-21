ee34e7e64f8b8dc98282654f754b0c98ead26a3e
Amber Thrall <amber.rose.thrall@gmail.com>
2024-03-21 16:04:26 -0700

    feat: Smith normal form

--- src/boundary_matrix.rs
+++ src/boundary_matrix.rs
@@ -3,6 +3,7 @@ use super::{
 };
 use nalgebra as na;
 
+/// Represents a boundary matrix of Z_2
 pub struct BoundaryMatrix {
     pub col_labels: Vec<Chain>,
     pub row_labels: Vec<Chain>,
@@ -37,44 +38,107 @@ impl BoundaryMatrix {
             m
         })
     }
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::{Complex, splx, Chain};
+    pub fn swap_columns(&mut self, i: usize, j: usize) {
+        // Swap the matrix columns
+        let coli = self.m.column(i).clone_owned();
+        let colj = self.m.column(j).clone_owned();
+        self.m.set_column(i, &colj);
+        self.m.set_column(j, &coli);
 
-    fn print_chain(chain: &Chain, complex: &Complex) {
-        if chain.is_zero() { print!("0"); }
-        for (i, h) in chain.0.iter().enumerate() {
-            if i > 0 { print!("+") } 
-            print!("{}", complex.get_simplex(*h).unwrap());
-        }
+        // Swap the labels
+        let coli = self.col_labels[i].clone();
+        let colj = self.col_labels[j].clone();
+        self.col_labels[i] = colj;
+        self.col_labels[j] = coli;
     }
 
-    #[test]
-    fn boundary_matrix() {
-        let mut complex = Complex::new();
-        complex.push(splx![0,1]);
-        complex.push(splx![0,2]);
-        complex.push(splx![1,2]);
-        complex.push(splx![1,3]);
-        complex.push(splx![1,4]);
-        complex.push(splx![2,3]);
-        complex.push(splx![3,4]);
-        complex.push(splx![0,1,2]);
-        complex.push(splx![1,3,4]);
-        
-        let soln = na::DMatrix::from_row_slice(5, 7, &[
-            1, 1, 0, 0, 0, 0, 0,
-            1, 0, 1, 1, 1, 0, 0,
-            0, 1, 1, 0, 0, 1, 0,
-            0, 0, 0, 1, 0, 1, 1,
-            0, 0, 0, 0, 1, 0, 1,
-        ]);
+    pub fn swap_rows(&mut self, i: usize, j: usize) {
+        // Swap the matrix columns
+        let rowi = self.m.row(i).clone_owned();
+        let rowj = self.m.row(j).clone_owned();
+        self.m.set_row(i, &rowj);
+        self.m.set_row(j, &rowi);
 
-        let bmatrix = BoundaryMatrix::new(1, &complex).unwrap();
-        println!("Solution: {}", soln);
-        println!("Got: {}", bmatrix.m);
+        // Swap the labels
+        let rowi = self.row_labels[i].clone();
+        let rowj = self.row_labels[j].clone();
+        self.row_labels[i] = rowj;
+        self.row_labels[j] = rowi;
+    }
+
+    /// Performs column replacement, C_i <- C_i + C_j
+    pub fn add_column(&mut self, i: usize, j: usize) {
+        // Add the matrix columns
+        let colj = self.m.column(j).clone_owned(); 
+        let mut coli = self.m.column_mut(i); 
+
+        for k in 0..coli.len() { 
+            let v = if coli[k] == colj[k] { 0 } else { coli[k] + colj[k] };
+            coli[k] = v;        
+        }
+
+        // Add the labels
+        let colj = self.col_labels[j].clone();
+        self.col_labels[i] += colj;
+    }
+
+    /// Performs row replacement, R_i <- R_i + R_j
+    pub fn add_row(&mut self, i: usize, j: usize) {
+        // Add the matrix rows
+        let rowj = self.m.row(j).clone_owned(); 
+        let mut rowi = self.m.row_mut(i); 
+
+        for k in 0..rowi.len() { 
+            let v = if rowi[k] == rowj[k] { 0 } else { rowi[k] + rowj[k] };
+            rowi[k] = v;        
+        }
+
+        // Add the labels
+        let rowj = self.row_labels[j].clone();
+        self.row_labels[i] += rowj;
+    }
+
+    /// Performs the Smith normal form algorthm
+    pub fn snf(&mut self) {
+        self.reduce(0);
+    }
+
+    fn reduce(&mut self, i: usize) {
+        let m = self.row_labels.len();
+        let n = self.col_labels.len();
+
+        // If there is some j>=i and k>=i such that m[j,k] == 1
+        let mut found = false;
+        'outer: for j in i..m {
+            for k in i..n {
+                if self.m[(j,k)] == 1 {
+                    // swap rows i & j and columns i & k
+                    found = true;
+                    self.swap_rows(i, j);
+                    self.swap_columns(i, k);
+                    break 'outer;
+                }
+            }
+        }
+
+        if found {
+            // Zero out 1's below m[i,i]
+            for h in (i+1)..m {
+                if self.m[(h,i)] == 1 {
+                    self.add_row(h, i);
+                }
+            }
+            
+            // Zero out 1's to the right of m[i,i]
+            for h in (i+1)..n {
+                if self.m[(i,h)] == 1 {
+                    self.add_column(h, i);
+                }
+            }
+
+            // Proceed to next block
+            self.reduce(i+1);
+        }
     }
 }
